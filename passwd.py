e99d40c61b941fa5770640624e31e0e631397d30
Varshithreddy <varshithreddy402@gmail.com>
2023-09-27 12:11:05 +0530

    added passwd.py

--- passwd.py
+++ passwd.py
@@ -1,48 +1,21 @@
-import string
 import random
+import string
 
-# Getting password length
-length = int(input("Enter password length: "))
+def generate_password(length=12):
+    # Define the character sets for the password
+    lowercase_letters = string.ascii_lowercase
+    uppercase_letters = string.ascii_uppercase
+    digits = string.digits
+    special_characters = "!@#$%^&*()_+[]{}|;':,.<>?`~"
 
-print('''Choose character set for password from these :
-		1. Digits
-		2. Letters
-		3. Special characters
-		4. Exit''')
+    # Combine all character sets
+    all_characters = lowercase_letters + uppercase_letters + digits + special_characters
 
-characterList = ""
+    # Generate a password with random characters from the combined set
+    password = ''.join(random.choice(all_characters) for _ in range(length))
 
-# Getting character set for password
-while(True):
-	choice = int(input("Pick a number "))
-	if(choice == 1):
-		
-		# Adding letters to possible characters
-		characterList += string.ascii_letters
-	elif(choice == 2):
-		
-		# Adding digits to possible characters
-		characterList += string.digits
-	elif(choice == 3):
-		
-		# Adding special characters to possible
-		# characters
-		characterList += string.punctuation
-	elif(choice == 4):
-		break
-	else:
-		print("Please pick a valid option!")
+    return password
 
-password = []
-
-for i in range(length):
-
-	# Picking a random character from our
-	# character list
-	randomchar = random.choice(characterList)
-	
-	# appending a random character to password
-	password.append(randomchar)
-
-# printing password as a string
-print("The random password is " + "".join(password))
+# Example usage: Generate a 16-character password
+password = generate_password(16)
+print(password)
